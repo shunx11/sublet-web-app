8e31d8628aa7ec33f0badad7737489ad3a4551fb
Daniel Xiao/Yu Xiao <91526326+shunx11@users.noreply.github.com>
2021-09-28 05:55:26 -0400

    Add files via upload

---
+++ App.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import './App.css';
+import Home from './Home'
+import Header from './Header'
+
+function App() {
+  return (
+
+    // BEM
+    <div className="app">
+      <Header />
+      <Home />
+
+      {/* Home */}
+         {/* Header */}
+
+         {/* Banner */}
+            {/* Search */}
+
+         {/* Cards */}
+      
+      {/* Search */}
+          {/* Header */}
+          {/* ... */}
+    </div>
+  );
+}
+
+export default App;
